2101413f311f810475560e42b91c85df02bf46d1
sumanth1030 <sumanth.mahilamuri@zaggle.in>
2022-06-23 04:15:28 +0000

    comments added

--- src/main/java/com/example/demo/services/CategoryServices.java
+++ src/main/java/com/example/demo/services/CategoryServices.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
+//Service
 @Service
 public class CategoryServices {
     @Autowired
